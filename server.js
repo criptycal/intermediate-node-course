77f5e9a0d94758dd8f0e1f27c9a3ebf5cc5febae
laEsquinaLlanera <laesquinallanera333@gmail.com>
2021-08-03 10:37:51 -0500

    addign get y post requests

--- server.js
+++ server.js
@@ -1,33 +1,65 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const port = 8000;
+const app = express();
 
 const User = require('./models/User');
 mongoose.connect('mongodb://localhost/userData');
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users', (req, res) => {
   // User.create()
+  User.create({
+    name: req.body.newData.name,
+    email: req.body.newData.email,
+    password: req.body.newData.password
+  },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err })
+      } else if (!data) {
+        res.json({ success: false, message: 'not found' })
+      } else {
+        res.json({ success: true, data: data })
+      }
+    })
 })
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+  // READ
+  .get((req, res) => {
+    // User.findById()
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: 'not found'
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    })
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  })
